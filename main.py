2daf0ef83eded4a414da9a844e1faf21376a5fd4
Kaiming Kuang <kaiming.kuang@outlook.com>
2018-07-19 21:24:28 -0400

    Add comments

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import pandas as pd
-from utilities import *
+import pandas as pd             # use Pandas to output csv file
+from utilities import *         # self-made utility functions to crawl the website
 
-statementDict = {}
+statementDict = {}              # create an empty dictionary to store the data
 
-for i in range(1, 18):
+for i in range(1, 18):          # there are 17 pages of statements on the website. We loop over all pages to get the data
     page = i
-    urlList = GetUrlList(page)
+    urlList = GetUrlList(page)  # get the urls from the current page
 
-    for url in urlList:
+    for url in urlList:         # loop over all urls on the current page
+        # some certain rules to filter out useless statements
         if (len(re.findall(r".*fad-press-release.*", url)) != 0 or len(re.findall(r".*rate.*", url)) != 0):
             print(url)
-            statement, _, strDate = GetReleaseAndDate(url)
-            statementDict[strDate] = statement
+            statement, date = GetReleaseAndDate(url)      # extract the statement and the release date
+            statementDict[date] = statement                  # put the data into the dictionary
 
+# extract the keys and the values in the dictionary separately
 keys = []
 values = []
 for key, value in statementDict.items():
     keys.append(key)
     values.append(value)
 
+# output the data
 output = pd.DataFrame(values, index=keys)
 print(output.info())
-output.to_csv("statements.csv", index=True)
+output.to_csv("BankOfCanada.csv", index=True)
