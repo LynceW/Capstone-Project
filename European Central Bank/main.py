6fd94df1566cd760f07b93206d15abdac5c9e97d
Kaiming Kuang <kaiming.kuang@outlook.com>
2018-07-20 14:25:27 -0400

    Update main.py

--- European Central Bank/main.py
+++ European Central Bank/main.py
@@ -1,19 +1,20 @@
-from utilities import *
-import pandas as pd
+from utilities import *         # self-made functions to perform scrapping
+import pandas as pd             # use pandas to output csv file
 
 
-statementList = []
+statementList = []              # create empty list to store statements and dates
 dateList = []
-for year in range(1999, 2019):
-    urlList = GetUrlList(year)
 
-    for url in urlList:
-        print(url)
-        date = GetDate(url)
-        string = Url2Str(url)
-        statement = Str2Statement(string)
-        statementList.append(statement)
-        dateList.append(date)
+for year in range(1999, 2019):          # the records are from 1999 to 2018
+    urlList = GetUrlList(year)          # get a list of all urls
 
-output = pd.DataFrame({"Date": dateList, "Statement": statementList})
+    for url in urlList:                     # loop over all urls on the list
+        print(url)                          # print the url to track the progress
+        date = GetDate(url)                 # extract the publication date
+        string = Url2Str(url)               # convert the url content to string
+        statement = Str2Statement(string)   # extract the statement
+        statementList.append(statement)     # add the statement and the date
+        dateList.append(date)               # to the list
+
+output = pd.DataFrame({"Date": dateList, "Statement": statementList})       # output the data as .csv file
 output.to_csv("EuropeanCentralBank.csv", index=False)
